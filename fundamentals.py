c0dc3e80c742e14fb409470b9a33ee58306412b0
Shellpro954 <20kmacijauska.acc@lwetb.ie>
2024-10-22 13:27:26 +0100

    Add files via upload

---
+++ fundamentals.py
@@ -0,0 +1,101 @@
+z = int(input("how many days "))
+x = int(input("how many days "))
+y = int(input("how many days "))
+
+days1 = x * y * z
+days2 = x * y
+days3 = y * z
+days4 = x * z
+
+total = days1 / (days2 + days3 + days4)
+
+print (total)
+
+number1 = int(input("pick any integer "))
+number2 = int(input("pick another integer "))
+
+addtion = number1 + number2
+minus = number1 - number2
+times = number1 * number2
+divide = number1 / number2
+
+print ("addtion ", addtion)
+print ("subtraction ", minus)
+print ("multipled ", times)
+print ("divided ", divide)
+
+var1 = "3"
+var2 = "5"
+
+print (var1, var2)
+
+var1= int(input("pick a number to change var 1 and 2 "))
+var2 = var1
+print (var1, var2)
+
+var1 = int(3)
+var2 = int(5)
+
+print (var1, var2)
+
+var1 = var1 - 52359
+var2 = var1
+print(var1, var2)
+
+num1 = float(input("pick a number "))
+num2  = float(input("pick another number "))
+num3 =  float(input("pick your last number "))
+
+avg = (num1 + num2 + num3) / 3
+print (avg)
+
+sphere1 = int(7)
+sphere2 = int(12)
+sphere3 = int(16)
+pi = float(3.14)
+
+spherevol1 = 1.33333333 * pi * sphere1 ** 3
+print (spherevol1)
+
+spherevol2 = 1.3333333 * pi * sphere2 ** 3
+print (spherevol2)
+
+spherevol3 = 1.33 * pi * sphere3 ** 3
+print (spherevol3)
+
+name = input("what is your name ")
+age = int(input("how old are you "))
+year = (2024 - age) + 100
+print (name , "you will turn 100 in the year",year)
+
+mass = float(input("what is the mass of your object "))
+energy = mass * (3 * 108) ** 2
+print ("your object has", energy,"energy")
+
+name = input("what is your name? ")
+Class = input("what class are you in? ")
+adress = input("what is your address? ")
+rollno = input("what is your roll num? ")
+section = "A"
+city = input("what city are you from? ")
+pincode = input("what is your pin code? ")
+phonenum = input("what is your parents or guardians phone number? ")
+schoolname = input("what is your school called")
+print( "\t","\t", schoolname )
+print("Name:",name,"\t","\t","\t", "Roll No:", rollno)
+print("Class:",Class,"\t","\t","\t", "Section:", section)
+print("Address", "\t", adress)
+print(" ")
+print("City:", "\t",city, "\t","\t", "Pincode:",pincode)
+print("Parents / Guardians phone number", phonenum)
+
+
+
+
+
+           
+
+
+
+           
+
